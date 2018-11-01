1654999e519d71b9a2489fd59221565cc801cbc9
Drew Fyock <fyockm@gmail.com>
2018-11-01 12:02:45 -0400

    bring master into parity with v1.0 (#9)
    * bring master into parity with v1.0
    
    * Find appropriate module version when multiple available

--- test/lib/externals.test.js
+++ test/lib/externals.test.js
@@ -9,8 +9,8 @@ describe('externals', () => {
         expect(externals).to.be.an('object');
         expect(externals.compatible).to.be.an('object');
         expect(externals.incompatible).to.be.an('object');
-        expect(externals.compatible.async).to.be.equal('async@1.0.0');
-        expect(externals.compatible.auth0).to.be.equal('auth0@0.8.2');
+        expect(externals.compatible.async).to.be.equal('async@2.1.2');
+        expect(externals.compatible.auth0).to.be.equal('auth0@2.9.1');
         expect(externals.incompatible.boom).to.be.an('object');
         expect(externals.incompatible.else).to.be.an('object');
         done();
